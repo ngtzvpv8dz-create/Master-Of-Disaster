b72b74e5c99f981deca78e69b459a98aa3836580
ngtzvpv8dz-create <ngtzvpv8dz@privaterelay.appleid.com>
2026-08-18 21:38:31 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://oktpzwhhndsbikkeelot.supabase.co/rest/v1/",
+  url: "https://oktpzwhhndsbikkeelot.supabase.co",
   publishableKey: "sb_publishable_EaxtFXKAGOLyI6HRWPU6DQ_f04wfWru"
 };
