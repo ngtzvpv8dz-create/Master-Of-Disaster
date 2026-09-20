881a8866e8698b428a6df632ecfbd5dc18fd8bcf
ngtzvpv8dz-create <ngtzvpv8dz@privaterelay.appleid.com>
2026-09-20 20:03:15 +0200

    V603: cache consolidated To-do stability core

--- sw.js
+++ sw.js
@@ -20,6 +20,7 @@ const CORE_SHELL=[
   "./kistology-v603.css",
   "./kistology-v603.js",
   "./log-core-v603.js",
+  "./todo-stability-v603.js",
   "./backstage-v531.css",
   "./backstage-content-v531.css",
   "./backstage-size-v532.css",
